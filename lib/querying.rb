442ebd926e7f16c15fdb01d1d0a78e75305c9a31
Brock Byrd <brockbyrdd@gmail.com>
2020-10-06 22:15:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,8 @@ def select_series_title_with_most_human_characters
     INNER JOIN authors ON Authors.id = Series.author_id
     INNER JOIN characters ON Authors.id = Characters.author_id
     GROUP BY characters.species
-    "
+    ORDER BY species WHERE species = 'human'
+    DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
