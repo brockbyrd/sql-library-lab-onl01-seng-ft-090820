a3b390f727e4d05e08cca0f5ca4d8d167baa25bd
Brock Byrd <brockbyrdd@gmail.com>
2020-10-06 22:09:49 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT name, subgenres.name
+  "SELECT authors.name, subgenres.name
     FROM authors
     INNER JOIN series ON Authors.id = Series.author_id
     INNER JOIN subgenres ON Subgenres.id = Series.subgenre.id"
